75c8a174b581bc155d03be5b53b5cc3b0bc62cb8
crist <cristiandreyes97@gmail.com>
2022-08-05 17:54:33 -0500

    Ciclos o Bucles

---
+++ src/CicloFor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CicloFor {
+/*
+Ciclo for
+
+    for (inicializacion; condicion; aumento o decremento){
+        Instruccion;
+}
+ */
+    public static void main(String[] args) {
+
+        Scanner para = new Scanner(System.in);
+
+        int contador3;
+
+        System.out.print("Ingrese la cantidad de números para imprimir: ");
+        contador3 = para.nextInt();
+
+        for (int i = 2; i <= contador3; i += 2){
+            System.out.print(i+" - ");
+        }
+
+    }
+}
