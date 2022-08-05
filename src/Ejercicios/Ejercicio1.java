e7e5179744655f066f64510a7211f848a3b2a32d
crist <cristiandreyes97@gmail.com>
2022-08-05 17:56:47 -0500

    Ciclos o Bucles

---
+++ src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+/*
+Leer un número y mostrar su cuadrado, repetir el proceso hasta que se intruduzca un número negativo
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+    }
+}
