1174549a5f283f5275711ba593e08d46f9eb58a4
crist <cristiandreyes97@gmail.com>
2022-08-06 16:55:47 -0500

    Ciclos o Bucles

--- src/Ejercicios/Ejercicio1.java
+++ src/Ejercicios/Ejercicio1.java
@@ -1,4 +1,7 @@
 package Ejercicios;
+
+import javax.swing.*;
+
 /*
 Leer un número y mostrar su cuadrado, repetir el proceso hasta que se intruduzca un número negativo
  */
@@ -6,5 +9,17 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
 
+        int numero, cuadrado;
+
+        numero = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un número: "));
+
+        while (numero >= 0){
+            cuadrado = (int) Math.pow(numero,2);
+
+            JOptionPane.showMessageDialog(null,"El número es: "+numero+ " elevado al cuadrado es: "+cuadrado);
+
+            numero = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese otro número: "));
+
+        }
     }
 }
